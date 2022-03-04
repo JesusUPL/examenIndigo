0ec8244765d36d94691d5aeda681002f5ca76c0f
jesus <jeslet.fres@gmail.com>
2022-03-03 22:37:16 -0600

    se crea persona con propiedades

---
+++ spring-boot-indigo/src/main/java/com/indigo/springbootindigo/SpringBootIndigoApplication.java
@@ -0,0 +1,13 @@
+package com.indigo.springbootindigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootIndigoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootIndigoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/indigo/springbootindigo/Dto/Persona.java
@@ -0,0 +1,94 @@
+package com.indigo.springbootindigo.Dto;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.lang.NonNull;
+
+@Entity
+@Table(name = "personas")
+public class Persona implements Serializable{
+	
+	private static final long serialVersionUID = -4839806247658244742L;
+
+
+	public Persona() {
+	
+	}
+	
+	public Persona(String nombre, int edad, String sexo, String codigo, Date creacionRegistro) {
+		super();
+		this.nombre = nombre;
+		this.edad = edad;
+		this.sexo = sexo;
+		this.codigo = codigo;
+		this.creacionRegistro = creacionRegistro;
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY )
+	private Long id;
+	
+	@NonNull
+	@NotBlank
+	private String nombre;
+	
+	private int edad;
+	
+	@NotNull
+	@NotBlank
+	private String sexo;
+	
+	@NotNull
+	@NotBlank
+	private String codigo;
+	
+	private Date creacionRegistro;
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	public String getSexo() {
+		return sexo;
+	}
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+	public String getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+	public Date getCreacionRegistro() {
+		return creacionRegistro;
+	}
+	public void setCreacionRegistro(Date creacionRegistro) {
+		this.creacionRegistro = creacionRegistro;
+	} 
+
+}
